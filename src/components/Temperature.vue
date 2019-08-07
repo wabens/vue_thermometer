6d4d395f76a32f14a2932466cd5c652225a369ae
wabens <waltkbenson@gmail.com>
2019-08-07 10:21:13 -0500

    created template for temperature

---
+++ src/components/Temperature.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class = "temperature">
+    :min = "min"
+    :max = "max"
+    :style = "{background: color}"
+    :value = "value"
+    @input = "handleInput"
+    type = "range"
+
+    <div class = "labels">
+      <div>{{ min }}</div>
+        <div>temperature: {{ value }}F</div>
+      <div>{{ max }}</div> 
+    </div>
+
+    <div class = "evaluation">
+      {{ evaluation }}
+    </div>
+
+  </div>
+</template>
+
